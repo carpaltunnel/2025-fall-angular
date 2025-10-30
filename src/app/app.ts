0a8573e5efebcaf89b15262b49676c01ad95f226
CC <a_fake_address@uca.edu>
2025-10-29 20:42:17 -0500

    cleanup

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { ChickenSearch } from './chicken-search/chicken-search';
-import { Chickens } from './chickens/chickens';
-import { Chicken } from './chicken/chicken';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Chickens, Chicken, ChickenSearch],
+  imports: [RouterOutlet, ChickenSearch],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
